da065ae5843cbc21c8522bb69f5bd1ea1e212812
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-24 15:02:56 -0400

    feat: Add UserButton component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div>
+      <Button>Click me</Button>
+      <UserButton />
+    </div>
   );
 }
