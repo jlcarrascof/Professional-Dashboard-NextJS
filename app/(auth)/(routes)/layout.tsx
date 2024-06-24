977e0a33fc23a998850196830cc20b7e70765976
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-24 18:48:58 -0400

    chore: Update LayoutAuth component to include children

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,6 +1,6 @@
 export default function LayoutAuth({ children }: { children: React.ReactNode }) {
   return (
-    <div>LayoutAuth
+    <div className="flex flex-col justify-center h-full items-center">
         {children}
     </div>
   )
