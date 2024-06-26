87d6f1a8950cba0eea9ee4b5e82b98fea4bdf6bf
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-26 19:34:14 -0400

    Update Home page to include Dashboard heading and card summaries

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div>
       <UserButton />
-      <h2>Dashboard</h2>
+      <h2 className="text-2xl mb-4">Dashboard</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+      </div>
     </div>
   );
 }
