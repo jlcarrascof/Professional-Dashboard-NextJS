8eb37c0380a1855d95df11bd420fa50122c0c0c8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-26 18:55:38 -0400

    chore: Update LayoutAuth component to include welcome message

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,6 +1,9 @@
 export default function LayoutAuth({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col justify-center h-full items-center">
+        <p>Tarredev Dashboard</p>
+        <h1 className="text-3xl my-2">Welcome to my dashboard</h1>
+        <h2 className="text-2xl mb-3">TarreDashboard</h2>
         {children}
     </div>
   )
