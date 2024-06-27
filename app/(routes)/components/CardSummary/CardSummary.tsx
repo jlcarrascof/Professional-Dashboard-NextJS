0302c1c039cfda280facc034cf26472ee66238ad
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-26 21:49:34 -0400

    refactor: Update CardSummary component to improve styling and structure

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -4,6 +4,10 @@ export function CardSummary(props: CardSummaryProps) {
   const { icon, total, average, title, tooltipText } = props;
 
   return (
-    <div>CardSummary</div>
+    <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
+        <div className="flex justify-between">
+            {title}
+        </div>
+    </div>
   )
 }
