4238ddfb16a405881142602586e691e713ab3d92
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-27 13:00:02 -0400

    feat: Add @radix-ui/react-tooltip dependency and implement CustomTooltip component

--- components/CustomTooltip/CustomTooltip.tsx
+++ components/CustomTooltip/CustomTooltip.tsx
@@ -1,5 +1,26 @@
-export function CustomTooltip() {
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip";
+import { CustomTooltipProps } from "./CustomTooltip.types";
+import { Info } from "lucide-react";
+
+export function CustomTooltip(props: CustomTooltipProps) {
+
+  const { content } = props;
+
   return (
-    <div>CustomTooltip</div>
+    <TooltipProvider>
+        <Tooltip>
+            <TooltipTrigger>
+                <Info strokeWidth={1} className="w-5 h-5" />
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>Add to library</p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
   )
 }
