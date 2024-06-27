fe9fa97e5d8e5f9a6ea3a2bdb68422a4ec046671
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-26 20:36:51 -0400

    feat: Update CardSummary component to include icon, total, average, title, and tooltipText props

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
 import { CardSummary } from "./components/CardSummary/CardSummary";
+import { UsersRound } from "lucide-react";
 
 export default function Home() {
   return (
@@ -8,7 +9,13 @@ export default function Home() {
       <UserButton />
       <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <CardSummary />
+        <CardSummary
+          icon={UsersRound}
+          total="12.450"
+          average={15}
+          title="Total Companies"
+          tooltipText="This is the total number of companies in your dashboard"
+        />
         <div>Card Summary</div>
         <div>Card Summary</div>
       </div>
