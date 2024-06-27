bfc25d5fab21e28f4ab1911ff400c95fa2911c1b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-27 00:50:22 -0400

    refactor: Update CardSummary component to include CustomIcon and props for icon, total, average, title, and tooltipText

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,11 +1,13 @@
+import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 
 export function CardSummary(props: CardSummaryProps) {
-  const { icon, total, average, title, tooltipText } = props;
+  const { icon: Icon, total, average, title, tooltipText } = props;
 
   return (
     <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
         <div className="flex justify-between">
+            <CustomIcon icon={Icon} />
             {title}
         </div>
     </div>
