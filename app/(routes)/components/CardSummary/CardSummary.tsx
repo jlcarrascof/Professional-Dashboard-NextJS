90e5c42cfdf459304718bd783416761077bec7a8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-26 20:33:19 -0400

    feat: Update CardSummary component to accept props for icon, total, average, title, and tooltipText

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,4 +1,8 @@
-export function CardSummary() {
+import { CardSummaryProps } from "./CardSummary.types";
+
+export function CardSummary(props: CardSummaryProps) {
+  const { icon, total, average, title, tooltipText } = props;
+
   return (
     <div>CardSummary</div>
   )
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
+import { CardSummary } from "./components/CardSummary/CardSummary";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <UserButton />
       <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <div>Card Summary</div>
+        <CardSummary />
         <div>Card Summary</div>
         <div>Card Summary</div>
       </div>
